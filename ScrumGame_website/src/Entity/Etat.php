aecacd6f26a8cb587c0a7819995a181b320685c2
Hugo Poquet <hugo.poquet@icloud.com>
2022-12-08 11:26:01 +0100

    Maj marques etats

---
+++ ScrumGame_website/src/Entity/Etat.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EtatRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EtatRepository::class)
+ */
+class Etat
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nomEtat;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $descriptionEtat;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomEtat(): ?string
+    {
+        return $this->nomEtat;
+    }
+
+    public function setNomEtat(string $nomEtat): self
+    {
+        $this->nomEtat = $nomEtat;
+
+        return $this;
+    }
+
+    public function getDescriptionEtat(): ?string
+    {
+        return $this->descriptionEtat;
+    }
+
+    public function setDescriptionEtat(string $descriptionEtat): self
+    {
+        $this->descriptionEtat = $descriptionEtat;
+
+        return $this;
+    }
+}
